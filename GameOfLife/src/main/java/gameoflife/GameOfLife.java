b2ac54d7da0054749872e1fb9c7bf8e6aa6b3cf5
nicusorb@gmail.com <nicolae@123>
2013-11-17 15:42:03 +0200

    Implemented the first three rules of game of live (see http://codingdojo.org/cgi-bin/wiki.pl?KataGameOfLife)

--- GameOfLife/src/main/java/gameoflife/GameOfLife.java
+++ GameOfLife/src/main/java/gameoflife/GameOfLife.java
@@ -1,5 +1,7 @@
 package gameoflife;
 
+import java.util.Arrays;
+
 public class GameOfLife {
     private final boolean[][] cells;
 
@@ -12,15 +14,27 @@ public class GameOfLife {
     }
 
     public boolean[][] nextGeneration() {
+        boolean[][] newCells = cloneCells();
+
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++) {
                 int nbOfLiveNeighbours = getNumberOfAliveNeighbours(i, j);
 
-                if (nbOfLiveNeighbours == 0 && cells[i][j])
-                    cells[i][j] = false;
+                if (nbOfLiveNeighbours < 2 && cells[i][j]) //underpopulation
+                    newCells[i][j] = false;
+                else if (nbOfLiveNeighbours > 3 && cells[i][j]) //overcrowding
+                    newCells[i][j] = false;
             }
         }
-        return cells;
+        return newCells;
+    }
+
+    private boolean[][] cloneCells() {
+        boolean newCells[][] = new boolean[cells.length][cells.length];
+        for (int i = 0; i < cells.length; i++) {
+            newCells[i] = Arrays.copyOf(cells[i], cells[i].length);
+        }
+        return newCells;
     }
 
     private int getNumberOfAliveNeighbours(int x, int y) {
@@ -41,6 +55,6 @@ public class GameOfLife {
     }
 
     private boolean isValidPosition(int pos) {
-        return pos > 0 && pos < cells.length - 1;
+        return pos >= 0 && pos <= cells.length - 1;
     }
 }
--- GameOfLife/src/test/java/gameoflife/GameOfLifeTest.java
+++ GameOfLife/src/test/java/gameoflife/GameOfLifeTest.java
@@ -16,4 +16,58 @@ public class GameOfLifeTest {
 
         assertThat(cells[1][1], is(false));
     }
+
+    @Test
+    public void anyLiveCellWithLessThanTwoAliveNeighbours_diesInNextGeneration() throws Exception {
+        sut.alive(0, 1);
+        sut.alive(1, 1);
+
+        boolean[][] cells = sut.nextGeneration();
+
+        assertThatAllCellsAreDeath(cells);
+    }
+
+    @Test
+    public void anyLiveCellWithMoreThanThreeNeighbours_diesInNextGeneration() throws Exception {
+        sut.alive(0, 0);
+        sut.alive(0,1);
+        sut.alive(1,1);
+        sut.alive(1,0);
+        sut.alive(1,2);
+
+        boolean[][] cells = sut.nextGeneration();
+
+        assertThat(cells[1][1], is(false));
+    }
+
+    @Test
+    public void anyLiveCellWithTwoNeighbours_livesInNextGeneration() throws Exception {
+        sut.alive(0, 1);
+        sut.alive(1, 1);
+        sut.alive(1, 2);
+
+        boolean[][] cells = sut.nextGeneration();
+
+        assertThat(cells[1][1], is(true));
+    }
+
+    @Test
+    public void anyLiveCellWithThreeNeighbours_livesInNextGeneration() throws Exception {
+        sut.alive(0, 1);
+        sut.alive(1, 0);
+        sut.alive(1, 1);
+        sut.alive(1, 2);
+
+        boolean[][] cells = sut.nextGeneration();
+
+        assertThat(cells[1][1], is(true));
+    }
+
+    private void assertThatAllCellsAreDeath(boolean[][] cells) {
+        for (boolean[] row : cells) {
+            for (boolean cell : row) {
+                assertThat(cell, is(false));
+            }
+        }
+    }
 }
