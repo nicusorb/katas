b3f038e8423178da64740a14e406a409b0754896
nicusorb@gmail.com <nicolae@123>
2014-02-01 10:20:25 +0200

    More tests added

--- WordWrap/src/main/java/org/katas/WordWrap.java
+++ WordWrap/src/main/java/org/katas/WordWrap.java
@@ -2,6 +2,27 @@ package org.katas;
 
 public class WordWrap {
     public static String wrap(String s, int columnLength) {
-        return s;
+        if (lineFitsTheColumnLength(s, columnLength))
+            return s;
+        if (s.charAt(columnLength) == ' ') {
+            return s.substring(0, columnLength) + "\n" +
+                    wrap(s.substring(columnLength + 1), columnLength);
+        }
+
+        int lastIndexOfSpace = s.substring(0, columnLength).lastIndexOf(' ');
+        if (lastIndexOfSpace != -1) {
+            String firstLine = s.substring(0, lastIndexOfSpace);
+            return firstLine + "\n" + wrap(s.substring(lastIndexOfSpace), columnLength);
+        } else {
+            int firstIndexOfSpace = s.substring(columnLength).indexOf(' ');
+            if (firstIndexOfSpace != -1) {
+                return s.substring(0, firstIndexOfSpace) + '\n' + wrap(s.substring(firstIndexOfSpace), columnLength);
+            }
+            return s;
+        }
+    }
+
+    private static boolean lineFitsTheColumnLength(String s, int columnLength) {
+        return s.length() <= columnLength;
     }
 }
--- WordWrap/src/test/java/org/katas/WordWrapTest.java
+++ WordWrap/src/test/java/org/katas/WordWrapTest.java
@@ -17,5 +17,39 @@ public class WordWrapTest {
         assertThat(wrap("abcd", 2), is("abcd"));
     }
 
+    @Test
+    public void doNotWrap_ifSmallerThanLength() throws Exception {
+        assertThat(wrap("ab c", 4), is("ab c"));
+    }
 
+    @Test
+    public void doNotWrap_ifFitsTheLength() throws Exception {
+        assertThat(wrap("ab cd", 5), is("ab cd"));
+    }
+
+    @Test
+    public void wrapLine_ifContainsOneSpace() throws Exception {
+        assertThat(wrap("ab cd", 2), is("ab\ncd"));
+    }
+
+    @Test
+    public void wrapLine_ifContainsMultipleSpaces() throws Exception {
+        assertThat(wrap("abcd abcd abcd", 4), is("abcd\nabcd\nabcd"));
+    }
+
+    @Test
+    public void wrapLineToTheFirstSpaceBeforeColumnLength() throws Exception {
+        assertThat(wrap("ab cd e", 3), is("ab\ncd\ne"));
+    }
+
+    @Test
+    public void wrapAfterColumnLength_ifNoSpacesAreFound() throws Exception {
+        assertThat(wrap("abcd abc", 3), is("abcd\nabc"));
+    }
+
+    @Test
+    public void doNotWrapLinesWithSpaces_ifFitTheLength() throws Exception {
+        assertThat(wrap("abcd a  bc", 5), is("abcd\na  bc"));
+        assertThat(wrap("a  d a  bc", 5), is("a  d\na  bc"));
+    }
 }
